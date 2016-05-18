c547580af32e1d63f9719186543388509ee57b5b
Gregor <gregor@martynus.net>
2016-05-17 23:37:45 -0500

    fix: set ENV.baseUrl and ENV.rootUrl to `/hoodie/admin/` for production

--- config/environment.js
+++ config/environment.js
@@ -40,7 +40,8 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-
+    ENV.baseURL = '/hoodie/admin/';
+    ENV.rootUrl = '/hoodie/admin/';
   }
 
   return ENV;
