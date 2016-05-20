8f5cd93182316613e887f3c8cf7d50f37ea0f45d
Gregor <gregor@martynus.net>
2016-05-20 12:30:35 -0400

    chore: remove unneeded fingerprint built option

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,9 +4,7 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    fingerprint: {
-      prepend: '/hoodie/admin/'
-    }
+
   });
 
   // Use `app.import` to add additional libraries to the generated
